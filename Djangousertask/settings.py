e24063c7380fd458a1f6ca154c1aacdee8361cf3
safinmnabi <marhat.rambo@gmail.com>
2021-12-02 22:24:41 +0600

    Pop

--- Djangousertask/settings.py
+++ Djangousertask/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-q!wl!8ne0b_1nsad)rwo((3vtl)rl!^p71bfo24rfb)%%k9@i1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['20.119.55.244']
+ALLOWED_HOSTS = ['20.119.55.244', '127.0.0.1']
 
 
 # Application definition
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'apps'
 ]
 
 MIDDLEWARE = [
@@ -74,15 +75,25 @@ WSGI_APPLICATION = 'Djangousertask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+#         'NAME': 'intimedb2',
+#         'USER': 'intime2',
+#         'PASSWORD': 'abc123456',
+#         'HOST': 'localhost',   
+#         'PORT': '5432',
+#     }    
+# }
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-        'NAME': 'intimedb2',
-        'USER': 'intime2',
-        'PASSWORD': 'abc123456',
-        'HOST': 'localhost',   
-        'PORT': '5432',
-    }    
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_PATH, 'database.sqlite'),
+    }
+
 }
 
 
@@ -123,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
